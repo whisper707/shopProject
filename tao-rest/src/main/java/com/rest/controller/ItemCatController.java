0e5fae68eafbddf067c22daf6069a507c67baedd
lzy <lzy@126.com>
2020-03-13 23:30:41 +0800

    version 0313_3.0 rest的jsonp已经完成 修改了上个版本递归的问题

--- tao-rest/src/main/java/com/rest/controller/ItemCatController.java
+++ tao-rest/src/main/java/com/rest/controller/ItemCatController.java
@@ -4,14 +4,32 @@ import com.rest.pojo.CatResult;
 import com.rest.pojo.JsonUtils;
 import com.rest.service.ItemCatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ItemCatController {
     @Autowired
     ItemCatService itemCatService;
 
+    @RequestMapping(value="/itemcat/list",
+            produces=MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+    @ResponseBody
+
     public String  getItemCatList(String callback){
+        CatResult catResult = itemCatService.getItemCatList();
+        //转型json
+        String json = JsonUtils.objectToJson(catResult);
+        //转jsonp
+        String data = callback + "(" + json + ");";
+        return data;
+    }
+    @RequestMapping(value="/t")
+    @ResponseBody
+    public String  t(String callback){
+        System.out.println("t");
         CatResult catResult = itemCatService.getItemCatList();
         //转型json
         String json = JsonUtils.objectToJson(catResult);
--- tao-rest/src/main/java/com/rest/service/impl/ItemCatServiceImpl.java
+++ tao-rest/src/main/java/com/rest/service/impl/ItemCatServiceImpl.java
@@ -32,19 +32,58 @@ public class ItemCatServiceImpl implements ItemCatService {
         List<CatNode> clist = new ArrayList<>();
         for (TbItemCat t : list) {
             CatNode c = new CatNode();
+            //name
             if (parentId == 0) {
                 c.setName("<a href='/products/"+t.getId()+".html'>"+t.getName()+"</a>");
             } else {
                 c.setName(t.getName());
             }
+            //url
             c.setUrl("/products/"+t.getId()+".html");
+            //item
             if (t.getIsParent()){
-                c.setItem(this.getCatnodelist(t.getParentId()));
-            }else {
-                c.setItem(null);
+                c.setItem(getCatnodelist(t.getId()));
             }
             clist.add(c);
         }
         return clist;
     }
+    /**
+     * 查询分类列表
+     * <p>Title: getCatList</p>
+     * <p>Description: </p>
+     * @param parentId
+     * @return
+     */
+    private List<?> getCatList(long parentId) {
+        //创建查询条件
+        TbItemCatExample example = new TbItemCatExample();
+        TbItemCatExample.Criteria criteria = example.createCriteria();
+        criteria.andParentIdEqualTo(parentId);
+        //执行查询
+        List<TbItemCat> list = tbItemCatMapper.selectByExample(example);
+        //返回值list
+        List resultList = new ArrayList<>();
+        //向list中添加节点
+        for (TbItemCat tbItemCat : list) {
+            //判断是否为父节点
+            if (tbItemCat.getIsParent()) {
+                CatNode catNode = new CatNode();
+                if (parentId == 0) {
+                    catNode.setName("<a href='/products/"+tbItemCat.getId()+".html'>"+tbItemCat.getName()+"</a>");
+                } else {
+                    catNode.setName(tbItemCat.getName());
+                }
+                catNode.setUrl("/products/"+tbItemCat.getId()+".html");
+                catNode.setItem(getCatList(tbItemCat.getId()));
+
+                resultList.add(catNode);
+                //如果是叶子节点
+            } else {
+                resultList.add("/products/"+tbItemCat.getId()+".html|" + tbItemCat.getName());
+            }
+        }
+        return resultList;
+    }
+
 }
